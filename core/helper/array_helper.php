506063e23bf06a4a13565c916f02a5fdd9816b1e
richardoentaryo <richardoentaryo@gmail.com>
2017-04-26 22:13:33 +0700

    Create array_helper.php

---
+++ core/helper/array_helper.php
@@ -0,0 +1,17 @@
+<?php
+
+    function getElements($items, array $array, $default = NULL)
+	{
+		$return = array();
+
+		is_array($items) OR $items = array($items);
+
+		foreach ($items as $item)
+		{
+			$return[$item] = array_key_exists($item, $array) ? $array[$item] : $default;
+		}
+
+		return $return;
+	}
+
+?>
