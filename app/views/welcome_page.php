e74eebb7366cc188b41f264315ea4344e730c90f
richardoentaryo <richardoentaryo@gmail.com>
2017-04-26 22:11:52 +0700

    Create welcome_page.php

---
+++ app/views/welcome_page.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+    <!--
+        use the command below to use the Bootstrap API/CDN web interface
+        <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        use the command below to use the Bootstrap from css directory
+        <link rel="stylesheet" type="text/css" href="../app/resources/css/bootstrap.min.css">
+    -->
+    <link rel="stylesheet" type="text/css" href="../app/resources/css/bootstrap.min.css">
+
+</head>
+<body>
+    <br>
+    <hr>
+    <div class="jumbotron text-center">
+        <h1> Welcome to FPF! </h1>
+        <p> Facade PHP Framework, a very simple MVC framework skeleton for PHP. </p>
+    </div>
+    <hr>
+
+</body>
+</html>
