29a66ae9deaf5688ac97fb343c904ec959fac03c
richardoentaryo <richardoentaryo@gmail.com>
2017-04-26 22:05:45 +0700

    Create welcome_model.php

---
+++ app/model/welcome_model.php
@@ -0,0 +1,30 @@
+<?php
+
+class Welcome_model extends Model
+{
+	public function __construct()
+	{
+		parent::__construct();
+		//add your own contructor below...
+	}
+
+	public function fetch()
+	{
+		//select code...
+	}
+
+	public function insert()
+	{
+		//insert code...
+	}
+
+	public function update()
+	{
+		//update code...
+	}
+
+	public function delete()
+	{
+		//delete code...
+	}
+}
