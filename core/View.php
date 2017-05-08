3ee8be1440da634550c20ba280b7e16a25705167
richardoentaryo <richardoentaryo@gmail.com>
2017-05-08 10:44:21 +0700

    Update View.php

--- core/View.php
+++ core/View.php
@@ -40,10 +40,20 @@ class View
             extract($data);
         }
 
-        require_once Config::get("VIEWS_PATH") . 'header/' . $layoutName . '_header.php';
-        require_once Config::get("VIEWS_PATH")  . $layoutName . '.php';
-        require_once Config::get("VIEWS_PATH") . 'footer/' . $layoutName . '_footer.php';
-        return ob_get_clean();
+        if( file_exists(Config::get("VIEWS_PATH") . 'header/' . $headerName . '.php') &&
+            file_exists(Config::get("VIEWS_PATH")  . $bodyName . '.php') &&
+            file_exists(Config::get("VIEWS_PATH") . 'footer/' . $footerName . '.php'))
+        {
+            ob_start();
+            require_once Config::get("VIEWS_PATH") . 'header/' . $headerName . '.php';
+            require_once Config::get("VIEWS_PATH")  . $bodyName . '.php';
+            require_once Config::get("VIEWS_PATH") . 'footer/' . $footerName . '.php';
+            ob_end_flush();
+        }
+        else
+        {
+            echo "Error! Could not completely found view template!";
+        }
     }
 }
 
