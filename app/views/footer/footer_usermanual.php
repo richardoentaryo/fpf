10b1555fa6a3b48816bf804065bca735695227dc
richardoentaryo <richardoentaryo@gmail.com>
2017-08-16 21:37:37 +0700

    Uploaded user manual footer template

---
+++ app/views/footer/footer_usermanual.php
@@ -0,0 +1,18 @@
+    </div>
+</div>
+<!-- end of content section -->
+
+<!-- footer section -->
+<footer style="background-color:#e6fff9;">
+    <center>
+    <div class="container">
+        <div class="col-lg-12">
+            <br><p><strong>Copyright &copy; Facade PHP Framework 2017</strong></p><br>
+        </div>
+    </div>
+    </center>
+</footer>
+<!-- end of footer section -->
+
+</body>
+</html>
