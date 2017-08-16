08b7a252ebf552c433b222a46a01ac7debf731e8
richardoentaryo <richardoentaryo@gmail.com>
2017-08-16 21:42:07 +0700

    Added user manual page handling

--- app/controllers/welcome.php
+++ app/controllers/welcome.php
@@ -11,4 +11,22 @@ class Welcome extends Controller
 	{
 		$this->view->forge("welcome_page");
 	}
+
+	public function usermanual($guidePage = null, $menuChanged = null)
+	{
+		if ($menuChanged != null)				 $this->redirect("welcome/usermanual/" . $menuChanged);
+
+		if ($guidePage == null) 			 $this->view->forgeWithLayouts("header_usermanual", "userManual/userManual", "footer_usermanual");
+		else if ($guidePage == "cli") 		 $this->view->forgeWithLayouts("header_usermanual", "userManual/CLI_manual", "footer_usermanual");
+		else if ($guidePage == "controller") $this->view->forgeWithLayouts("header_usermanual", "userManual/controller_manual", "footer_usermanual");
+		else if ($guidePage == "model") 	 $this->view->forgeWithLayouts("header_usermanual", "userManual/model_manual", "footer_usermanual");
+		else if ($guidePage == "view") 		 $this->view->forgeWithLayouts("header_usermanual", "userManual/view_manual", "footer_usermanual");
+		else if ($guidePage == "config") 	 $this->view->forgeWithLayouts("header_usermanual", "userManual/config_manual", "footer_usermanual");
+		else if ($guidePage == "session") 	 $this->view->forgeWithLayouts("header_usermanual", "userManual/session_manual", "footer_usermanual");
+		else if ($guidePage == "cookie") 	 $this->view->forgeWithLayouts("header_usermanual", "userManual/cookies_manual", "footer_usermanual");
+		else if ($guidePage == "validation") $this->view->forgeWithLayouts("header_usermanual", "userManual/validation_manual", "footer_usermanual");
+		else if ($guidePage == "encryption") $this->view->forgeWithLayouts("header_usermanual", "userManual/encryption_manual", "footer_usermanual");
+		else if ($guidePage == "upload") 	 $this->view->forgeWithLayouts("header_usermanual", "userManual/upload_manual", "footer_usermanual");
+		else if ($guidePage == "helpers") 	 $this->view->forgeWithLayouts("header_usermanual", "userManual/helper_manual", "footer_usermanual");
+	}
 }
