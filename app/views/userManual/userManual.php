7351e793b0d7ec14d16674d86e25bcb128732c8e
richardoentaryo <richardoentaryo@gmail.com>
2017-08-16 21:34:38 +0700

    Create userManual.php

---
+++ app/views/userManual/userManual.php
@@ -0,0 +1,18 @@
+<div class="col-md-9">
+    <p class="lead">
+        <b>Description</b>
+    </p>
+
+    <div class="row" id="demo">
+
+        <div class="panel panel-default">
+            <div class="panel-heading"><b>Well... this is a simple use guide</b></div>
+            <div class="panel-body">
+                We hope this guide will help you to like Facade PHP framework. <br>
+                Let's start by clicking an item from the menu.
+            </div>
+        </div>
+
+    </div>
+
+</div>
