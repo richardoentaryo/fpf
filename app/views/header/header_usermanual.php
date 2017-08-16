9d65f3bd51d2871cae28b3b10d369cb06c046ba3
richardoentaryo <richardoentaryo@gmail.com>
2017-08-16 21:36:45 +0700

    Uploaded user manual header template

---
+++ app/views/header/header_usermanual.php
@@ -0,0 +1,72 @@
+<html>
+<head>
+    <!--
+        use the command below to use the Bootstrap API/CDN web interface
+        <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        use the command below to use the Bootstrap from css directory
+        <link rel="stylesheet" type="text/css" href="../app/resources/css/bootstrap.min.css">
+    -->
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <title>FPF User Manual</title>
+
+</head>
+<body style="background-color:#ccfff2;">
+
+<!-- header section -->
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a class="navbar-brand"><i><b><font size=7>FPF</font></b></i></a>
+        </div>
+        <ul class="nav navbar-nav">
+            <li><a><font size=4>Facade PHP Framework - version 1.4</font></a></li>
+        </ul>
+    </div>
+</nav>
+<!-- end of header section -->
+
+<!-- beginning of content section -->
+<div class="container">
+
+    <div class="row">
+
+        <!-- beginning of menu section -->
+        <div class="col-md-3">
+            <p class="lead"><b>Framework Features</b></p>
+            <div class="list-group">
+                <a href="usermanual/cli" class="list-group-item">
+                    <h4 class="list-group-item-heading">Command Line Interface</h4>
+                </a>
+                <a href="usermanual/controller" class="list-group-item">
+                    <h4 class="list-group-item-heading">Controller</h4>
+                </a>
+                <a href="usermanual/model" class="list-group-item">
+                    <h4 class="list-group-item-heading">Model</h4>
+                </a>
+                <a href="usermanual/view" class="list-group-item">
+                    <h4 class="list-group-item-heading">View</h4>
+                </a>
+                <a href="usermanual/config" class="list-group-item">
+                    <h4 class="list-group-item-heading">Configurations</h4>
+                </a>
+                <a href="usermanual/session" class="list-group-item">
+                    <h4 class="list-group-item-heading">Sessions</h4>
+                </a>
+                <a href="usermanual/cookie" class="list-group-item">
+                    <h4 class="list-group-item-heading">Cookies</h4>
+                </a>
+                <a href="usermanual/validation" class="list-group-item">
+                    <h4 class="list-group-item-heading">Validations</h4>
+                </a>
+                <a href="usermanual/encryption" class="list-group-item">
+                    <h4 class="list-group-item-heading">Encryptions</h4>
+                </a>
+                <a href="usermanual/upload" class="list-group-item">
+                    <h4 class="list-group-item-heading">File Upload</h4>
+                </a>
+                <a href="usermanual/helpers" class="list-group-item">
+                    <h4 class="list-group-item-heading">Helpers</h4>
+                </a>
+            </div>
+        </div>
+        <!-- end of menu section -->
